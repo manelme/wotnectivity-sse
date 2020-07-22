1e338a88df685c7fee5ec116a24849e568ab9fa0
Manel <manel.mena@ual.es>
2020-07-22 12:14:57 +0200

    1.0.1 version done

--- README.md
+++ README.md
@@ -14,20 +14,42 @@ foo@bar:~$ npm i wotnectivity-knx --save
 
 Check if the `package.json` of your project contains the library as a depencency.
 
-## Use Example
+## Examples
 
-In the next fragment of code you can see a use case of `KnxReq`.
+In the next fragments of code you can see different use cases of `KNX` requests.
 
-```node
-const wotnectivity-knx = require("wotnectivity-knx");
+* Read a value from the group address 3/0/2.
+```js
+var Wotnectivity = require("wotnectivity-knx");
+ 
+var address = "192.168.1.38";
+var payload = 20;
+var configuration = { requestType: "read", group: "3/0/2", dataType: "DPT5.001" };
 
-var address = "192.168.1.62";
-var payload = "0.1";
-var configuration = {requestType: "write", dataType:"5.001", group:"1/0/1"};
-
-wotnectivity-knx.sendRequest(addres, configuration, payload).then((data)=>{
+Wotnectivity.sendRequest(address, configuration ,payload).then((data)=>{
     console.log(data)
-})
+});
+
+```
+* Write a value in the group address 3/0/2.
+```js
+var Wotnectivity = require("wotnectivity-knx");
+ 
+var address = "192.168.1.38";
+var payload = 20;
+var configuration = { requestType: "write", group: "3/0/2", dataType: "DPT5.001" };
+
+Wotnectivity.sendRequest(address, configuration ,payload).then((data)=>{
+    console.log(data)
+});
+
+```
+* Subscribe to the group address 3/0/3 to see the changes of the values in said address.
+```js
+var sub = await sendRequest("192.168.1.38", { requestType: "subscribe", group: "3/0/3", dataType: "DPT5.001" }, null)
+  sub.subscribe((data) => {
+    console.log(data);
+  });
 ```
 
 The configuration parameter of the request needs to have at least three parameters: 
--- src/index.js
+++ src/index.js
@@ -1,123 +1,114 @@
 var knx = require('knx');
 var rxjs = require('rxjs');
 
-var connection;
-
-
 async function sendRequest(address, configuration, payload) {
-  if (address) {
-    if (configuration.requestType == "read") {
-      return new Promise((resolve, reject) => {
-        this.connection = knx.Connection({
-          ipAddr: address,
-          handlers: {
-            connected: () => {
-              readRequest(configuration).then(data => {
-                resolve(data)
-              }).catch((error) => {
-                reject(error)
-              })
+  try {
+    var connection;
+    if (address) {
+      if (configuration.requestType == "read") {
+        return new Promise((resolve, reject) => {
+          connection = knx.Connection({
+            ipAddr: address,
+            handlers: {
+              connected: () => {
+                readRequest(connection, configuration).then(data => {
+                  resolve(data)
+                  connection.Disconnect()
+                }).catch((error) => {
+                  reject(error)
+                  connection.Disconnect()
+                })
+              }
             }
-          }
+          })
         })
-      })
 
-    }
-    else if (configuration.requestType == "write") {
-      return new Promise((resolve, reject) => {
-        this.connection = knx.Connection({
-          ipAddr: address,
-          handlers: {
-            connected: () => {
-              writeRequest(configuration, payload).then(data => {
-                resolve(data)
-              }).catch((error) => {
-                reject(error)
-              })
+      }
+      else if (configuration.requestType == "write") {
+        return new Promise((resolve, reject) => {
+          connection = knx.Connection({
+            ipAddr: address,
+            handlers: {
+              connected: () => {
+                writeRequest(connection, configuration, payload).then(data => {
+                  resolve(data)
+                  connection.Disconnect()
+                }).catch((error) => {
+                  reject(error)
+                  connection.Disconnect()
+                })
+              }
             }
-          }
+          })
         })
-      })
-    }
-    else if (configuration.requestType == "subscribe") {
-      return new Promise((resolve) => {
-        this.connection = knx.Connection({
-          ipAddr: address,
-          handlers: {
-            connected: () => {
-              resolve(subscribeRequest(configuration));
+      }
+      else if (configuration.requestType == "subscribe") {
+        return new Promise((resolve) => {
+          connection = knx.Connection({
+            ipAddr: address,
+            handlers: {
+              connected: () => {
+                resolve(subscribeRequest(connection, configuration));
+              }
             }
-          }
+          })
         })
-      })
+      }
     }
+  } catch (error) {
+    console.log(error);
   }
 
-  async function readRequest(configuration) {
-    return new Promise((resolve, reject) => {
-      try {
-        var dp = new knx.Datapoint({
-          ga: configuration.group,
-          dpt: configuration.dataType
-        }, this.connection);
-        dp.read((src, value) => {
-          resolve(value);
-        });
-      } catch (error) {
-        reject(error);
-      }
-    })
-  }
-
-  async function writeRequest(configuration, payload) {
-    return new Promise((resolve, reject) => {
-      try {
-        var dp = new knx.Datapoint({
-          ga: configuration.group,
-          dpt: configuration.dataType
-        }, this.connection);
-        dp.write(payload);
-        dp.read((src, value) => {
-          resolve(value)
-        })
-      } catch (error) {
-        reject(error);
-      }
-    })
-  }
-
-  function subscribeRequest(configuration) {
-    return rxjs.Observable.create((data) => {
-      try {
-        var dp = new knx.Datapoint({
-          ga: configuration.group,
-          dpt: configuration.dataType
-        }, this.connection)
-        dp.on('change', (oldValue, newValue) => {
-          data.next(newValue);
-        })
-      } catch (error) {
-        console.log(error)
-      }
-    })
-
-  }
 }
 
-
-async function testRead() {
-  var hola = await sendRequest("192.168.1.33", { requestType: "read", group: "2/0/1", dataType: "DPT1.001" }, null);
+async function readRequest(connection, configuration) {
+  return new Promise((resolve, reject) => {
+    try {
+      var dp = new knx.Datapoint({
+        ga: configuration.group,
+        dpt: configuration.dataType
+      }, connection);
+      dp.read((src, value) => {
+        resolve(value);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  })
 }
-async function testWrite() {
-  var hola = await sendRequest("192.168.1.33", { requestType: "write", group: "2/0/0", dataType: "DPT1.001" }, true);
-  console.log(hola)
+
+async function writeRequest(connection, configuration, payload) {
+  return new Promise((resolve, reject) => {
+    try {
+      var dp = new knx.Datapoint({
+        ga: configuration.group,
+        dpt: configuration.dataType
+      }, connection);
+      dp.write(payload);
+      dp.read((src, value) => {
+        resolve(value)
+      })
+    } catch (error) {
+      reject(error);
+    }
+  })
 }
 
-async function testSubscribe() {
-  var hola = await sendRequest("192.168.1.33", { requestType: "subscribe", group: "2/0/0", dataType: "DPT1.001" }, null)
-  hola.subscribe((data) => {
-    console.log(data);
-  });
+function subscribeRequest(connection, configuration) {
+  return rxjs.Observable.create((data) => {
+    try {
+      var dp = new knx.Datapoint({
+        ga: configuration.group,
+        dpt: configuration.dataType
+      }, connection)
+      dp.on('change', (oldValue, newValue) => {
+        data.next(newValue);
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
 }
 
-testSubscribe()
+module.exports.sendRequest = sendRequest
